c7e1a45f1ecb27704cedd0aadbd81bed696157ed
Hzg25 <1374056704@qq.com>
2025-06-25 18:31:47 +0800

    20250625-1

---
+++ src/test/java/com/onlineorder/onlineordersystem/OnlineOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineorder.onlineordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
